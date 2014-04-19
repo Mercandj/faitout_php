955209ec4e618667927aeb7cd1727bc75b8e261f
Mercandj <modjow.jm9@gmail.com>
2014-04-19 18:00:22 +0200

    1er upload

---
+++ inscription/index.php
@@ -0,0 +1,36 @@
+<?php
+
+	include_once 'Utilisateur.php';
+	include_once 'check_utilisateur_db.php';
+
+	$pseudo = $_GET['pseudo'];
+	$mot_de_passe = $_GET['mot_de_passe'];
+
+	// Connexion à la base de données
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=faitout', 'root', '');
+	}
+	catch(Exception $e)
+	{
+	die('Erreur : '.$e->getMessage());
+	}
+
+	$res = "";
+
+	if(utilisateur_existant($pseudo))
+	{
+		$res.='Ce nom d\'utilisateur est deja pris';
+	}
+	else
+	{
+	  $us = new Utilisateur($pseudo, $mot_de_passe);
+
+	  $req = $bdd->prepare($us->getinsert());
+	  $req->execute($us->getarray());
+	  $res.='Felicitations, vous avez bien ete enregistre: vous pouvez desormais vous connecter avec votre username et votre password';
+
+	}
+
+	echo $res;
+?>
