ab983a1f427f70282f9fc13668d8a111271789a6
Mercandj <modjow.jm9@gmail.com>
2014-04-20 11:18:24 +0200

    Ajout : liste messages

---
+++ utilisateurs/liste_messages.php
@@ -0,0 +1,22 @@
+<?php
+	// Connexion à la base de données
+	try {
+		$bdd = new PDO('mysql:host=localhost;dbname=faitout', 'root', '');
+	}
+	catch(Exception $e) {
+		die('Erreur : '.$e->getMessage());
+	}
+
+	$res = '{';
+
+	$req = $bdd->prepare('SELECT * FROM `message`');
+	$req->execute();
+	while($donnees = $req->fetch()) {
+		$res.='message = {';
+		$res.='"pseudo":"'.$donnees['pseudo'].'", ';
+		$res.='"message":"'.$donnees['message'].'"';
+		$res.='}';
+	}
+
+	echo $res.'}';
+?>
