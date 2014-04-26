232ca9230b77a60ddfb15428de12058c1771ea17
Mercandj <modjow.jm9@gmail.com>
2014-04-26 16:01:26 +0200

    liste des messages avec description user

--- utilisateurs/liste_messages.php
+++ utilisateurs/liste_messages.php
@@ -12,9 +12,25 @@
 	$req = $bdd->prepare('SELECT * FROM `message`');
 	$req->execute();
 	while($donnees = $req->fetch()) {
+
 		$res.='{';
+
+		$req2 = $bdd->prepare('SELECT * FROM `utilisateur` WHERE `pseudo` = ?');
+		$req2->execute(array($donnees['Utilisateur_pseudo']));
+		if($donnees2 = $req2->fetch()) {
+			$res.='"utilisateur": {';
+			$res.='"pseudo": "'.$donnees2['pseudo'].'", ';
+			$res.='"mot_de_passe": "'.$donnees2['mot_de_passe'].'", ';
+			$res.='"sexe":"'.$donnees2['sexe'].'", ';
+			$res.='"xp":"'.$donnees2['xp'].'", ';
+			$res.='"url_image_profil":"'.$donnees2['url_image_profil'].'", ';
+			$res.='"admin":"'.$donnees2['admin'].'"';
+			$res.='}, ';
+		}
+
 		$res.='"Utilisateur_pseudo": "'.$donnees['Utilisateur_pseudo'].'", ';
 		$res.='"message": "'.$donnees['message'].'", ';
+		$res.='"Image_url": "'.$donnees['Image_url'].'", ';
 		$res.='"destinataire": "'.$donnees['destinataire'].'"';
 		$res.='},';
 	}
