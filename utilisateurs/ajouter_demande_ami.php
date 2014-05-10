fbaf545e1bc723d447d28bc08c571050f8c0ae67
Jonathan <modjow.jm9@gmail.com>
2014-05-10 15:53:21 +0200

    Vérification de l'amitié

--- utilisateurs/ajouter_demande_ami.php
+++ utilisateurs/ajouter_demande_ami.php
@@ -15,13 +15,19 @@
 
 	$res = '';
 
-	$date = date('Y-m-d H:i:s');
+	$req = $bdd->prepare('SELECT * FROM `ami` WHERE ( `Utilisateur_pseudo` = ? AND `pseudo_ami` = ? ) OR ( `Utilisateur_pseudo` = ? AND `pseudo_ami` = ? ) ');
+	$req->execute(array($pseudo, $pseudo_ami, $pseudo_ami, $pseudo));
+	if($donnees = $req->fetch()) {
+		$res.='KO';
+	}
+	else {
+		$date = date('Y-m-d H:i:s');
 
-	$us = new DemandeAmi($pseudo, $date, $pseudo_ami);
+		$us = new DemandeAmi($pseudo, $date, $pseudo_ami);
 
-	$req = $bdd->prepare($us->getinsert());
-	$req->execute($us->getarray());
-	$res.='Felicitations, demande ami(e) ajoutée.';
-
+		$req = $bdd->prepare($us->getinsert());
+		$req->execute($us->getarray());
+		$res.='Felicitations, demande ami(e) ajoutée.';
+	}
 	echo $res;
 ?>
