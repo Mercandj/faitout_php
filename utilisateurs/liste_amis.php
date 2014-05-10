75e45ebc62b0fa6282f914f02bd70a63606caeda
Jonathan <modjow.jm9@gmail.com>
2014-05-10 16:26:05 +0200

    Ajout liste des amis

--- utilisateurs/liste_amis.php
+++ utilisateurs/liste_amis.php
@@ -14,14 +14,45 @@
 
 	$req = $bdd->prepare('SELECT * FROM `ami` WHERE `Utilisateur_pseudo` = ? OR `pseudo_ami` = ?');
 	$req->execute(array($pseudo, $pseudo));
+
+	$id = 0;
+
 	while($donnees = $req->fetch()) {
-		$res.='{';
-		$res.='"pseudo": "'.$donnees['pseudo'].'", ';
-		$res.='"mot_de_passe": "'.$donnees['mot_de_passe'].'", ';
-		$res.='"sexe":"'.$donnees['sexe'].'", ';
-		$res.='"xp":"'.$donnees['xp'].'", ';
-		$res.='"admin":"'.$donnees['admin'].'"';
-		$res.='},';
+
+		if($id!=0) {
+			$res.=',';
+		}
+		
+		if($donnees['Utilisateur_pseudo'] != $pseudo) {
+			$req2 = $bdd->prepare('SELECT * FROM `utilisateur` WHERE `pseudo` = ?');
+			$req2->execute(array($donnees['Utilisateur_pseudo']));
+			if($donnees2 = $req->fetch()) {
+				$res.='{';
+				$res.='"pseudo": "'.$donnees2['pseudo'].'", ';
+				$res.='"mot_de_passe": "'.$donnees2['mot_de_passe'].'", ';
+				$res.='"sexe":"'.$donnees2['sexe'].'", ';
+				$res.='"xp":"'.$donnees2['xp'].'", ';
+				$res.='"admin":"'.$donnees2['admin'].'"';
+				$res.='}';
+			}
+		}
+		else if($donnees['pseudo_ami'] != $pseudo) {
+			$req2 = $bdd->prepare('SELECT * FROM `utilisateur` WHERE `pseudo` = ?');
+			$req2->execute(array($donnees['pseudo_ami']));
+			if($donnees2 = $req->fetch()) {
+				$res.='{';
+				$res.='"pseudo": "'.$donnees2['pseudo'].'", ';
+				$res.='"mot_de_passe": "'.$donnees2['mot_de_passe'].'", ';
+				$res.='"sexe":"'.$donnees2['sexe'].'", ';
+				$res.='"xp":"'.$donnees2['xp'].'", ';
+				$res.='"admin":"'.$donnees2['admin'].'"';
+				$res.='}';
+			}
+		}
+		else {
+			$res.='KO';
+		}
+		$id+=1;
 	}
 
 	echo $res.']}';
