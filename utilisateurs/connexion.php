93fe09b515bd84e1248e8c1d6d2af9b1c901df7b
Jonathan <modjow.jm9@gmail.com>
2014-05-11 15:36:41 +0200

    test 13

--- utilisateurs/connexion.php
+++ utilisateurs/connexion.php
@@ -43,13 +43,15 @@
 			$res.='"nombre_messages":"'.$donnees5['total'].'", ';
 		}
 
+		
+		$req6 = $bdd->prepare('SELECT * FROM ( SELECT rownum as `indice`, `Utilisateur_pseudo`, ( COUNT(*) AS `nb_message` ) FROM `message` GROUP BY `Utilisateur_pseudo` ORDER BY `nb_message`) WHERE `Utilisateur_pseudo` = ?');
+		
 		/*
-		$req6 = $bdd->prepare('SELECT * FROM ( SELECT `Utilisateur_pseudo`, ( COUNT(`Utilisateur_pseudo`) AS `nb_message` ) FROM `message` GROUP BY `Utilisateur_pseudo` ORDER BY `nb_message`) WHERE `Utilisateur_pseudo` = ?');
-		*/
 		$req6 = $bdd->prepare('SELECT `Utilisateur_pseudo`, COUNT(*) AS nb_message FROM `message` WHERE `Utilisateur_pseudo` = ?');
+		*/
 		$req6->execute(array($pseudo));
 		while($donnees6 = $req6->fetch()) {
-			$res.='"chat_rang":"'.$donnees6['Utilisateur_pseudo'].' '.$donnees6['nb_message'].'", ';
+			$res.='"chat_rang":"'.$donnees6['Utilisateur_pseudo'].' '.$donnees6['nb_message'].' '.$donnees6['indice'].'", ';
 		}
 
 		$res.='"admin":"'.$donnees['admin'].'"';
