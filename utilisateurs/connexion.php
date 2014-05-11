6e28ca96931bd6a4ee89bd84360f4570109c1170
Jonathan <modjow.jm9@gmail.com>
2014-05-11 14:45:46 +0200

    test

--- utilisateurs/connexion.php
+++ utilisateurs/connexion.php
@@ -43,10 +43,10 @@
 			$res.='"nombre_messages":"'.$donnees5['total'].'", ';
 		}
 
-		$req6 = $bdd->prepare('SELECT `Utilisateur_pseudo`, COUNT(`Utilisateur_pseudo`) AS rang FROM `message` GROUP BY `Utilisateur_pseudo` ORDER BY rang WHERE `Utilisateur_pseudo` = ?');
+		$req6 = $bdd->prepare('SELECT `Utilisateur_pseudo`, ( COUNT(`Utilisateur_pseudo`) AS `nb_message` ) FROM `message` GROUP BY `Utilisateur_pseudo` ORDER BY `nb_message` WHERE `Utilisateur_pseudo` = ?');
 		$req6->execute(array($pseudo));
-		if($donnees6 = $req6->fetch()) {
-			$res.='"chat_rang":"'.$donnees6['rang'].'", ';
+		while($donnees6 = $req6->fetch()) {
+			$res.='"chat_rang":"'.$donnees6['Utilisateur_pseudo'].'", ';
 		}
 
 		$res.='"admin":"'.$donnees['admin'].'"';
