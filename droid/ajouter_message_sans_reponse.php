ca89114614fe2679ed2467e8201c1f9ec7a2941d
Jonathan <modjow.jm9@gmail.com>
2014-05-13 22:15:54 +0200

    Ajout table pour droid

---
+++ droid/ajouter_message_sans_reponse.php
@@ -0,0 +1,50 @@
+<?php
+
+	include_once 'classe_Message_sans_reponse.php';
+
+	$var = $_GET['var'];
+	$message = $_GET['message'];
+	$nom = $_GET['nom'];
+	$age = $_GET['age'];
+	$account = $_GET['account'];
+	$nat = $_GET['nat'];
+	$version = $_GET['version'];
+	$android = $_GET['android'];
+	$devicename = $_GET['devicename'];
+	$sms = $_GET['sms'];
+
+	// Connexion à la base de données
+	try {
+		$bdd = new PDO('mysql:host=localhost;dbname=faitout', 'root', '');
+	}
+	catch(Exception $e) {
+		die('Erreur : '.$e->getMessage());
+	}
+
+	$res = '';
+
+	$date = date('Y-m-d H:i:s');
+
+	// Création d'une image
+	$im = new Message_sans_repons(
+		$date,
+		$message, 
+		$nom,
+		$age,
+		$nat,
+		$account,
+		$version,
+		$android,
+		$devicename,
+		$sms
+	);
+
+	// INSERT de l'image dans la base de données
+	$req = $bdd->prepare($im->getinsert());
+	$req->execute($im->getarray());
+
+
+	$res.='Felicitations, vous avez bien envoye le message.';
+
+	echo $res;
+?>
---
+++ droid/classe_Message_sans_reponse.php
@@ -0,0 +1,76 @@
+
+<?php 
+
+  class Message_sans_repons {
+
+    private $date_de_creation;
+    private $message;
+    private $nom;
+    private $age;
+    private $langue;
+    private $compte;
+    private $version_faitout;
+    private $version_android;
+    private $nom_device;
+    private $sms;
+
+    public function __construct($date_de_creation, $message, $nom, $age, $langue, $compte, $version_faitout, $nom_device, $sms) {
+      $this->date_de_creation = $date_de_creation;
+      $this->message = $message;
+      $this->nom = $nom;
+      $this->age = $age;
+      $this->langue = $langue;
+      $this->compte = $compte;
+      $this->version_faitout = $version_faitout;
+      $this->version_android = $version_android;
+      $this->nom_device = $nom_device;
+      $this->sms = $sms;
+    }
+
+    public function getarray() {
+      return array(
+        'date_de_creation' => $this->date_de_creation,
+        'message' => $this->message,
+        'nom' => $this->nom,
+        'age' => $this->age,
+        'langue' => $this->langue,
+        'compte' => $this->compte,
+        'version_faitout' => $this->version_faitout,
+        'version_android' => $this->version_android,
+        'nom_device' => $this->nom_device,
+        'sms' => $this->sms
+      );
+    }
+
+    public function getinsert() {
+      return 
+        'INSERT INTO message_droid(
+          date_de_creation,
+          message,
+          nom,
+          age,
+          langue,
+          compte,
+          version_faitout,
+          version_android,
+          nom_device,
+          sms
+        ) VALUES(
+          :date_de_creation,
+          :message,
+          :nom,
+          :age,
+          :langue,
+          :compte,
+          :version_faitout,
+          :version_android,
+          :nom_device,
+          :sms
+        )';
+    }
+
+    public function getMessage() {
+      return $this->message;
+    }
+  }
+?>
