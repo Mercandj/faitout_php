a6e88422ace73d91ca296017ced5eb3630674758
Jonathan <modjow.jm9@gmail.com>
2014-06-03 16:26:21 +0200

    Utilisation du mur dans l'accueil

--- utilisateurs/connexion.php
+++ utilisateurs/connexion.php
@@ -175,8 +175,8 @@
 
 		$res .= '"messages" : [';
 
-		$req = $bdd->prepare('SELECT * FROM `message` WHERE `destinataire` = ? ORDER BY date_de_creation DESC LIMIT 30');
-		$req->execute(array('All'));
+		$req = $bdd->prepare('SELECT * FROM `message` WHERE `destinataire` = ? AND `pseudo` = ? ORDER BY date_de_creation DESC LIMIT 30');
+		$req->execute(array('Mur', $pseudo));
 
 		$x = 0;
 
