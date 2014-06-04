faacb53aeae93677105d58dbf5efaa33dbd83a14
Jonathan <modjow.jm9@gmail.com>
2014-06-04 12:15:35 +0200

    test

--- utilisateurs/liste_messages_droid.php
+++ utilisateurs/liste_messages_droid.php
@@ -9,7 +9,7 @@
 
 	$res = '{ "messages" : [';
 
-	$req = $bdd->prepare('SELECT * FROM `message_droid` ORDER BY date_de_creation DESC LIMIT 50');
+	$req = $bdd->prepare('SELECT * FROM `message_droid` ORDER BY date_de_creation DESC LIMIT 1');
 	$req->execute();
 
 	$x = 0;
