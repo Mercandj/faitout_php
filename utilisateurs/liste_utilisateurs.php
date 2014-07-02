ba85d25e7aa4eca36d6524842cb414b4adb7574a
Jonathan <modjow.jm9@gmail.com>
2014-07-02 15:18:58 +0200

    Compression de code : lise utilisateurs

--- utilisateurs/liste_utilisateurs.php
+++ utilisateurs/liste_utilisateurs.php
@@ -9,19 +9,40 @@
 
 	$res = '{ "utilisateurs" : [';
 
-	$req = $bdd->prepare('SELECT * FROM `utilisateur`');
-	$req->execute();
-	while($donnees = $req->fetch()) {
-		$res.='{';
-		$res.='"pseudo": "'.$donnees['pseudo'].'", ';
-		$res.='"mot_de_passe": "'.$donnees['mot_de_passe'].'", ';
-		$res.='"sexe":"'.$donnees['sexe'].'", ';
-		$res.='"xp":"'.$donnees['xp'].'", ';
-		$res.='"url_image_profil":"'.$donnees['url_image_profil'].'", ';
-		$res.='"description":"'.$donnees['description'].'", ';
-		$res.='"admin":"'.$donnees['admin'].'"';
-		$res.='},';
-	}
+	if(isset($_GET['pseudo'])) {
 
-	echo $res.']}';
+		$pseudo = $_GET['pseudo'];
+
+		$req = $bdd->prepare('SELECT * FROM `utilisateur` WHERE `pseudo` LIKE "%'.$pseudo.'%"');
+		$req->execute();
+		while($donnees = $req->fetch()) {
+			$res.='{';
+			$res.='"pseudo": "'.$donnees['pseudo'].'", ';
+			$res.='"mot_de_passe": "'.$donnees['mot_de_passe'].'", ';
+			$res.='"sexe":"'.$donnees['sexe'].'", ';
+			$res.='"xp":"'.$donnees['xp'].'", ';
+			$res.='"url_image_profil":"'.$donnees['url_image_profil'].'", ';
+			$res.='"admin":"'.$donnees['admin'].'"';
+			$res.='},';
+		}
+
+		echo $res.']}';
+	}
+	else {
+		$req = $bdd->prepare('SELECT * FROM `utilisateur`');
+		$req->execute();
+		while($donnees = $req->fetch()) {
+			$res.='{';
+			$res.='"pseudo": "'.$donnees['pseudo'].'", ';
+			$res.='"mot_de_passe": "'.$donnees['mot_de_passe'].'", ';
+			$res.='"sexe":"'.$donnees['sexe'].'", ';
+			$res.='"xp":"'.$donnees['xp'].'", ';
+			$res.='"url_image_profil":"'.$donnees['url_image_profil'].'", ';
+			$res.='"description":"'.$donnees['description'].'", ';
+			$res.='"admin":"'.$donnees['admin'].'"';
+			$res.='},';
+		}
+
+		echo $res.']}';
+	}
 ?>
