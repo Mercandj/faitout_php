03673b9c87701e4dfeec44dd766df556fa3dc1d0
Jonathan <modjow.jm9@gmail.com>
2014-07-03 13:24:10 +0200

    test 16

--- musiques/liste_musiques_test.php
+++ musiques/liste_musiques_test.php
@@ -15,33 +15,28 @@
 			$res.=', ';
 		$res.='{';
 		$res.='"url": "'.$var.'",';
-		$res.='"titre": "'.$var.'"';
 
 		if (strpos($var,'mp3')) {
 			echo $i.' <br />';
-		    //$fp = fopen($dir.$var, 'r'); 
 
 			$musicfile = fopen($dir.$var, 'r');
 			fseek($musicfile, -128, SEEK_END);
 
 			$tag = fread($musicfile, 3);
 
-			if($tag == "TAG")
-			{
-			$data["song"] = trim(fread($musicfile, 30));
-			$data["artist"] = trim(fread($musicfile, 30));
-			$data["album"] = trim(fread($musicfile, 30));
-			}
-			else
-			{
-			echo "MP3 file does not have any ID3 tag!";
-
+			if($tag == "TAG") {
+				$res.='"size": "'.($data["song"] = trim(fread($musicfile, 30))).'",';
+				$res.='"size": "'.($data["artist"] = trim(fread($musicfile, 30))).'",';
+				$res.='"size": "'.($data["album"] = trim(fread($musicfile, 30))).'",';
 			}
 
-			echo $i.' $m='.$m.'<br />';
-		    //fclose($fp);
+			$res.='"size": "'.filesize($dir.$var).'",';
+
+		    fclose($musicfile);
 		}
 		
+
+		$res.='"titre": "'.$var.'"';
 
 		/*$a = $m->get_metadata();
 		 
