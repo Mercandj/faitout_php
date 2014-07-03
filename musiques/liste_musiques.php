fbf5d54e141624a42682001df0cd9eb8412059ef
Jonathan <modjow.jm9@gmail.com>
2014-07-03 14:07:44 +0200

    Ajout de la taille des musiques

--- musiques/liste_musiques.php
+++ musiques/liste_musiques.php
@@ -11,6 +11,23 @@
 			$res.=', ';
 		$res.='{';
 		$res.='"url": "'.$var.'",';
+
+		if (strpos($var,'mp3')) {
+			$musicfile = fopen($dir.$var, 'r');
+			fseek($musicfile, -128, SEEK_END);
+
+			$tag = fread($musicfile, 3);
+
+			if($tag == "TAG") {
+				$res.='"song": "'.($data["song"] = trim(fread($musicfile, 30))).'",';
+				$res.='"artist": "'.($data["artist"] = trim(fread($musicfile, 30))).'",';
+				$res.='"album": "'.($data["album"] = trim(fread($musicfile, 30))).'",';
+			}			
+
+		    fclose($musicfile);
+		}	
+
+		$res.='"size": "'.filesize($dir.$var).'",';
 		$res.='"titre": "'.$var.'"';
 		$res.='}';
 		$i++;
