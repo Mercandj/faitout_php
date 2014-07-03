3527fe9e5ad158c283ddbf99a318aa73a419212b
Jonathan <modjow.jm9@gmail.com>
2014-07-03 11:21:40 +0200

    Ajout test pour la musique

---
+++ musiques/liste_musiques_test.php
@@ -0,0 +1,34 @@
+<?php
+	$dir = "./../../musiques/";
+	$files1 = scandir($dir);
+
+	$res = '{ "musiques" : [';
+
+	$i=0;
+
+	foreach($files1 as $var) {
+		if($i!=0) 
+			$res.=', ';
+		$res.='{';
+		$res.='"url": "'.$var.'",';
+		$res.='"titre": "'.$var.'"';
+
+
+		$m = new mp3file($dir.$var);
+		$a = $m->get_metadata();
+		 
+		if ($a['Encoding']=='Unknown')
+		    $res.='"url": "?",';
+		else if ($a['Encoding']=='VBR')
+			$res.='"url": "'.$a.'",';
+		else if ($a['Encoding']=='CBR')
+		    $res.='"url": "'.$a.'",';
+		unset($a);
+
+
+		$res.='}';
+		$i++;
+	}
+
+	echo $res.']}';
+?>
