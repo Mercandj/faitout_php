d7483016e1e6bc0785057383b5f56bb5d5684103
Jonathan <modjow.jm9@gmail.com>
2014-07-20 21:44:40 +0200

    Ajout pagination

--- utilisateurs/v37/liste_utilisateurs.php
+++ utilisateurs/v37/liste_utilisateurs.php
@@ -13,7 +13,7 @@
 
 		$pseudo = $_GET['recherche_pseudo'];
 
-		$req = $bdd->prepare('SELECT * FROM `utilisateur` WHERE `pseudo` LIKE "%'.$pseudo.'%"');
+		$req = $bdd->prepare('SELECT * FROM `utilisateur` WHERE `pseudo` LIKE "%'.$pseudo.'% IS NOT NULL LIMIT 0 , 200"');
 		$req->execute();
 		$i = 0;
 		while($donnees = $req->fetch()) {
@@ -33,8 +33,35 @@
 
 		echo $res.']}';
 	}
+	else if(isset($_GET['page'])) {
+
+		$pseudo = $_GET['recherche_pseudo'];
+		$page = $_GET['page'];
+
+		$req = $bdd->prepare('SELECT * FROM `utilisateur` WHERE `pseudo` LIKE "%'.$pseudo.'% IS NOT NULL LIMIT '.(1* ($page-1)).' , '.(5*$page).'"');
+		$req->execute();
+		$i = 0;
+		while($donnees = $req->fetch()) {
+			if($i!=0)
+				$res.=',';
+			$res.='{';
+			$res.='"pseudo": "'.$donnees['pseudo'].'", ';
+			$res.='"mot_de_passe": "'.$donnees['mot_de_passe'].'", ';
+			$res.='"sexe":"'.$donnees['sexe'].'", ';
+			$res.='"xp":"'.$donnees['xp'].'", ';
+			$res.='"url_image_profil":"'.$donnees['url_image_profil'].'", ';
+			$res.='"description":"'.$donnees['description'].'", ';
+			$res.='"admin":"'.$donnees['admin'].'"';
+			$res.='}';
+			$i+=1;
+		}
+
+		echo $res.']';
+		if($i!=0)
+			echo $res.', "next":'.($page+1).'}';
+	}
 	else {
-		$req = $bdd->prepare('SELECT * FROM `utilisateur`');
+		$req = $bdd->prepare('SELECT * FROM `utilisateur` IS NOT NULL LIMIT 0 , 200');
 		$req->execute();
 		$i = 0;
 		while($donnees = $req->fetch()) {
