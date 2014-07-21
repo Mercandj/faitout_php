dc85a66fbe61b41c3f25d80b1429cde204d481ce
Jonathan <modjow.jm9@gmail.com>
2014-07-21 11:34:47 +0200

    Enlevé mots de passe

--- utilisateurs/v37/connexion.php
+++ utilisateurs/v37/connexion.php
@@ -23,7 +23,7 @@
 	if($donnees = $req->fetch()) {
 		$res.='{';
 		$res.='"pseudo": "'.$donnees['pseudo'].'", ';
-		$res.='"mot_de_passe": "'.$donnees['mot_de_passe'].'", ';
+		//$res.='"mot_de_passe": "'.$donnees['mot_de_passe'].'", ';
 		$res.='"sexe":"'.$donnees['sexe'].'", ';
 		$res.='"xp":"'.$donnees['xp'].'", ';
 		$res.='"url_image_profil":"'.$donnees['url_image_profil'].'", ';
