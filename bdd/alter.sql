b62af09cf6ac959b5151fbad34cc1e0329d00ec8
Jonathan <modjow.jm9@gmail.com>
2014-08-02 13:13:08 +0200

    Fix date inscription

--- bdd/alter.sql
+++ bdd/alter.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `Utilisateur` ADD `date_de_connexion` DATETIME NULL
+ALTER TABLE `Utilisateur` DROP COLUMN `date_inscription`
--- utilisateurs/v37/changer_date_de_connexion.php
+++ utilisateurs/v37/changer_date_de_connexion.php
@@ -3,5 +3,8 @@
 		$date = date('Y-m-d H:i:s');
 	    $req = $bdd->prepare('UPDATE `utilisateur` SET `date_de_connexion` = ? WHERE `pseudo` = ?');
 		$req->execute(array($date, $pseudo));
+
+		$req = $bdd->prepare('UPDATE `utilisateur` SET `date_de_creation` = ? WHERE `pseudo` = ?');
+		$req->execute(array($date, $pseudo));
 	}
 ?>
