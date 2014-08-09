6729d480ca66419bf585971b942917bb7a4ad591
Jonathan <modjow.jm9@gmail.com>
2014-08-09 14:36:53 +0200

    Fix bug classement

--- utilisateurs/v37/liste_amis.php
+++ utilisateurs/v37/liste_amis.php
@@ -355,7 +355,7 @@
 						)';
 
 						$req7 = $bdd->prepare($sql_req_7);
-						$req7->execute(array($donnees2['pseudo'], $donnees2['pseudo']));
+						$req7->execute(array($donnees2['pseudo']));
 						if($donnees7 = $req7->fetch())
 							$res.='"rang_ami":"'.$donnees7['rang'].'", ';
 					}
