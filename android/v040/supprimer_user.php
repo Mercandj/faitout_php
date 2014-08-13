7d4f9c4e9e596fbe9bd8564a81e319f8e747f5a8
Jonathan <modjow.jm9@gmail.com>
2014-08-13 15:20:33 +0200

    Fix bug

--- android/v040/supprimer_user.php
+++ android/v040/supprimer_user.php
@@ -7,8 +7,8 @@
 		$user->pseudo = $_GET['pseudo'];
 	if(isset($_GET['mot_de_passe']))
 		$user->mot_de_passe = $_GET['mot_de_passe'];
-	if(isset($_GET['pseudo_a_supp']))
-		$pseudo_a_supp = $_GET['pseudo_a_supp'];
+	if(isset($_GET['pseudosupp']))
+		$pseudosupp = $_GET['pseudosupp'];
 
 	$request_body = file_get_contents('php://input');
 	$phpArray = json_decode($request_body, true);
@@ -37,22 +37,22 @@
 	$req->execute(array($user->pseudo, $user->mot_de_passe));
 	if($donnees = $req->fetch()) {
 		$req = $bdd->prepare('DELETE FROM `groupe` WHERE `Utilisateur_pseudo` = ?');
-		$req->execute(array($pseudo_a_supp));
+		$req->execute(array($pseudosupp));
 
 		$req = $bdd->prepare('DELETE FROM `demandeami` WHERE `pseudo_ami` = ? OR `Utilisateur_pseudo` = ?');
-		$req->execute(array($pseudo_a_supp, $pseudo_a_supp));
+		$req->execute(array($pseudosupp, $pseudosupp));
 
 		$req = $bdd->prepare('DELETE FROM `image` WHERE `Utilisateur_pseudo` = ?');
-		$req->execute(array($pseudo_a_supp));
+		$req->execute(array($pseudosupp));
 
 		$req = $bdd->prepare('DELETE FROM `message` WHERE `Utilisateur_pseudo` = ?');
-		$req->execute(array($pseudo_a_supp));
+		$req->execute(array($pseudosupp));
 
 		$req = $bdd->prepare('DELETE FROM `ami` WHERE `Utilisateur_pseudo` = ? OR `pseudo_ami` = ?');
-		$req->execute(array($pseudo_a_supp, $pseudo_a_supp));
+		$req->execute(array($pseudosupp, $pseudosupp));
 
 		$req = $bdd->prepare('DELETE FROM `utilisateur` WHERE `pseudo` = ?');
-		$req->execute(array($pseudo_a_supp));
+		$req->execute(array($pseudosupp));
 
 		$res = 'User supprimé.';
 	}
